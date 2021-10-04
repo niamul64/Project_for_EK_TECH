7af3dacb6d40047cf1c309e74617ba53306319df
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2021-10-04 09:40:58 +0600

    check point, ticker added

--- dean_institute/lib/screens/welcome_screen.dart
+++ dean_institute/lib/screens/welcome_screen.dart
@@ -1,22 +1,42 @@
+import 'package:dean_institute/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dean_institute/constants/constants.dart';
 import 'package:dean_institute/screens/elements/welcomeScreenBody.dart';
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
   static const String id = 'welcomeScreen';
-
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  controller=AnimationController(vsync: this, duration: Duration(seconds: 2), );
+  controller.forward();
+  controller.addListener(() {
+    setState(() {
+      if ((controller.value)==1.0) {
+        Navigator.pushNamed(context, LoginScreen.id);
+      }
+    });
+  });
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:base_red_color,
       body: SafeArea(
         child: Body(),
-
       ),
 
         bottomNavigationBar: Padding(
@@ -30,8 +50,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             textAlign: TextAlign.center,
           ),
         ),
-
-
     );
   }
 }
